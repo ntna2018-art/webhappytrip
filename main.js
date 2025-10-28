b6b8c48e1f5cba3f9f50574e600ca882b139c3ca
ntna2018-art <ntna2018@gmail.com>
2025-10-28 18:39:48 +0700

    Remove chatbase initialization code
    Removed the chatbase initialization and loading script.

--- main.js
+++ main.js
@@ -50,29 +50,3 @@ window.addEventListener('load', () => {
     setTimeout(() => { pre.remove(); }, 3000);
   }
 });
-(function () {
-  if (!window.chatbase || window.chatbase("getState") !== "initialized") {
-    window.chatbase = (...arguments) => {
-      if (!window.chatbase.q) {
-        window.chatbase.q = []
-      } window.chatbase.q.push(arguments)
-    };
-    window.chatbase = new Proxy(window.chatbase, {
-      get(target, prop) {
-        if (prop === "q") { return target.q } return (...args) => target(prop, ...args)
-      }
-    })
-  }
-  const onLoad = function () {
-    const script = document.createElement("script");
-    script.src = "https://www.chatbase.co/embed.min.js";
-    script.id = "wvI9k0r-6LrTOm_XaVFd2";
-    script.domain = "www.chatbase.co";
-    document.body.appendChild(script)
-  };
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", onLoad);
-  } else {
-    onLoad();
-  }
-})();
